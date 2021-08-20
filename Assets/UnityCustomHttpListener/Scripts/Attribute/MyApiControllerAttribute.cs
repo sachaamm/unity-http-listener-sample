f57d24072fd03347a1791cc48ea77275567f7fce
sachaamm <sachaamm@gmail.com>
2021-08-20 11:50:13 +0200

    git submodule

--- Assets/UnityCustomHttpListener/Scripts/Attribute/MyApiControllerAttribute.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace UnityCustomHttpListener.Scripts.Attribute
-{
-    public class MyApiControllerAttribute : System.Attribute
-    {
-        // Even I am just a decorator, it doesnt means im useless ! :)
-    }
-}
